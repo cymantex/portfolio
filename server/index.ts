2b01e56dfd4705d9a0f09ce1184bdc79d1b5b258
dv16sen <dv16sen@cs.umu.se>
2019-01-01 22:04:01 +0100

    Added classes for communicating with the Server and Database.

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,12 @@
-import {startApp} from "./setup/startApp";
-import settings from "./settings.json";
+import Server from "./setup/Server";
+import {databaseOptions, serverOptions} from "./settings";
+import Database from "./setup/Database";
+import {log} from "./utils/log";
 
-startApp(settings).catch(err => {
-    console.error(err);
-    process.exit(1);
+const database = new Database(databaseOptions);
+const server = new Server(serverOptions, database);
+
+server.start().catch(async err => {
+    log.error(err);
+    await server.stop();
 });
